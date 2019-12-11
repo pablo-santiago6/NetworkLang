1edd7728aa15116648141d3242d897a5ef1cc12e
Wiliel Florenciani <wiliel.florenciani@upr.edu>
2019-12-11 17:30:28 -0400

    Update networklang.py
    changed networklang for a cleaner look

--- networklang.py
+++ networklang.py
@@ -1,15 +1,11 @@
-import ply.yacc as yacc
-from networklex import tokens
-import os
-import re
-import Icode
 import networkyacc
 
-while(True):
-    try:
-        s = input('')
-        if s == "quit":
+if __name__ == '__main__':
+    while(True):
+        try:
+            s = input('NetworkLang> ')
+            if s == "quit":
+                break
+        except EOFError:
             break
-    except EOFError:
-        break
-    networkyacc.parser.parse(s)
+        networkyacc.parser.parse(s)
