0b5d5440112b71434451b75f483b869d637f023b
Wiliel Florenciani <wiliel.florenciani@upr.edu>
2019-12-11 22:15:32 -0400

    removed files & updated lex and yacc to be cleaner

--- networkyacc.py
+++ networkyacc.py
@@ -1,7 +1,8 @@
+#
+#   networkyacc.py
+#
 import ply.yacc as yacc
 from networklex import tokens
-import os
-import re
 import Icode
 
 
@@ -48,43 +49,3 @@ def p_error(p):
 	print("syntax error ", p)
 
 parser = yacc.yacc()
-
-"""
-def findFileY(dir):
-    listfil = []
-    filnum = ''
-    ans = False
-    count = 1
-    for base, dirs, files in os.walk(dir):
-        listfil.append(files)
-    for file in files:
-        print(str(count) + '. ' +file)
-        count = count+1
-
-    while not ans:
-        filnum = input('\nNumber of test: ')
-        for file in files:
-            if file == files[int(filnum)-1]:
-                ans = True
-                break
-
-    print('chose', files[int(filnum)-1])
-    print('printing contents of file\n')
-    return files[int(filnum)-1]
-
-if __name__ == '__main__':
-    directory = os.path.curdir + '\\test\\'
-    file = findFileY(directory)
-    test = directory+file
-    fp = open(test,'r')
-    c = fp.read()
-    print(c+'\n')
-    print('--------------------')
-    print('-- syntax results --')
-    print('--------------------\n')
-    fp.close()
-
-# global parser
-    result = parser.parse(c)
-    print(result)
-"""
